db6946b6c2a4fc2f78d979476e2b777b27d6a6b1
DHB <1084714805@qq.com>
2020-08-10 23:26:07 +0800

    format js

--- js/main.js
+++ js/main.js
@@ -1,103 +1,101 @@
 // set up the containers in treeLib config
-treeLib.buildConfig(['g1', 'g2']);
+treeLib.buildConfig(["g1", "g2"]);
 
 // global variable for dataset, passed to all d3 generators
 var FileData;
 
-$(document).ready(function(){
+$(document).ready(function () {
     menu.setupCheckBoxes();
 
     // initial acc is leaves, see menu.config.accumulated
     $("#checkBoxAccumulated").prop("checked", true);
-    $(".data-info-types-span.leaves").css('opacity', 1);
-    $(".data-info-types-span.size").css('opacity', .25);
+    $(".data-info-types-span.leaves").css("opacity", 1);
+    $(".data-info-types-span.size").css("opacity", 0.25);
 
-    $("#checkBoxAccumulated").on("click", function() {     
+    $("#checkBoxAccumulated").on("click", function () {
         var isChecked = $(this).prop("checked");
-        
-        if (isChecked){
-            menu.updateAccumulated('leaves');
-            $(".data-info-types-span.leaves").css('opacity', 1);
-            $(".data-info-types-span.size").css('opacity', .25);
-        } else { 
-            menu.updateAccumulated('size');
-            $(".data-info-types-span.leaves").css('opacity', .25);
-            $(".data-info-types-span.size").css('opacity', 1);
+
+        if (isChecked) {
+            menu.updateAccumulated("leaves");
+            $(".data-info-types-span.leaves").css("opacity", 1);
+            $(".data-info-types-span.size").css("opacity", 0.25);
+        } else {
+            menu.updateAccumulated("size");
+            $(".data-info-types-span.leaves").css("opacity", 0.25);
+            $(".data-info-types-span.size").css("opacity", 1);
         }
 
-        var locked1 = menu.isLocked('1');
-        var locked2 = menu.isLocked('2');
+        var locked1 = menu.isLocked("1");
+        var locked2 = menu.isLocked("2");
 
-        loadVisualization('1', locked1);
+        loadVisualization("1", locked1);
 
-        loadVisualization('2', locked2);
+        loadVisualization("2", locked2);
     });
 
-    $("#checkBoxRememberLayout1").on("click", function() {
-        menu.changeLockPosition('1');
+    $("#checkBoxRememberLayout1").on("click", function () {
+        menu.changeLockPosition("1");
     });
 
-    $("#checkBoxRememberLayout2").on("click", function() {
-        menu.changeLockPosition('2');
-    });    
+    $("#checkBoxRememberLayout2").on("click", function () {
+        menu.changeLockPosition("2");
+    });
 
     setupSliderValueTooltip();
     setupToolTips();
 });
 
-function Remove_nodechildren(id){
-    var parent = document.getElementById(id)
+function Remove_nodechildren(id) {
+    var parent = document.getElementById(id);
     var childrens = parent.childNodes;
-    if(childrens != ''){
-        for(var i = childrens.length-1; i >=0; i--)
-        {
-        parent.removeChild(childrens[i]);
+    if (childrens != "") {
+        for (var i = childrens.length - 1; i >= 0; i--) {
+            parent.removeChild(childrens[i]);
         }
     }
 }
 
-function change_tiling(type, position){
-    change_map(position)
-}       
+function change_tiling(type, position) {
+    change_map(position);
+}
 
-function clearVisualization(position){
-    d3.select("#g" + position+ "-div")
+function clearVisualization(position) {
+    d3.select("#g" + position + "-div")
         .html("")
         .append("svg")
         .attr("id", "g" + position)
         .attr("viewBox", "0 0 500 500");
 
-    d3.select("svg#g"+position).on('doneDrawing', function(){
+    d3.select("svg#g" + position).on("doneDrawing", function () {
         var objS = document.getElementById("dropdown" + position);
-        
+
         // var grade = objS.options[objS.selectedIndex].text;
         var grade = objS.value;
-        treeLib.updateConfig(grade,'g'+position);
+        treeLib.updateConfig(grade, "g" + position);
     });
 }
 
 // update both maps if one is changed
-function change_map(position){
+function change_map(position) {
     // unlock the changed map
     menu.unlockPosition(position);
 
     // which map was changed?
-    var position2 = position == '1' ? '2' : '1';
-    var position1 = position == '1' ? '1' : '2';
-    
+    var position2 = position == "1" ? "2" : "1";
+    var position1 = position == "1" ? "1" : "2";
+
     // resetCfg();
-    // reset both maps when one layout is changed 
+    // reset both maps when one layout is changed
     // remember to lock the choices???
     var locked1 = menu.isLocked(position1);
-    var locked2 = menu.isLocked(position2)
+    var locked2 = menu.isLocked(position2);
 
     loadVisualization(position1, locked1);
 
     loadVisualization(position2, locked2);
 }
 
-function loadVisualization(position, locked){
-
+function loadVisualization(position, locked) {
     clearVisualization(position);
 
     // hide specific tags for treemap and radial trees
@@ -110,63 +108,56 @@ function loadVisualization(position, locked){
     }
 
     var objS = document.getElementById("dropdown" + position);
-    
+
     var grade = objS.value;
 
-    if(grade == "Pack"){
+    if (grade == "Pack") {
         this.draw_pack("#g" + position);
-    }
-    else if(grade == "Sunburst"){
+    } else if (grade == "Sunburst") {
         this.draw_sunburst("#g" + position);
-    }
-    else if(grade == "Treemap"){
+    } else if (grade == "Treemap") {
         $("#layout" + position + "_treemap").removeClass("hide-tag");
 
         this.draw_treemap("#g" + position, document.getElementById("dropdown" + position + "_treemap").selectedIndex);
-    }
-    else if(grade == "Zoomable_Treemap"){
+    } else if (grade == "Zoomable_Treemap") {
         this.draw_zoomable_treemap("#g" + position);
-        
-    }
-    else if(grade == "Collapsible_Tree"){
+    } else if (grade == "Collapsible_Tree") {
         $("#nodeSizeDiv" + position + "_radialtree").removeClass("hide-tag");
         this.draw_collapsible_tree("#g" + position);
-    }
-    else if(grade == "Radial_Tree"){
+    } else if (grade == "Radial_Tree") {
         $("#nodeSizeDiv" + position + "_radialtree").removeClass("hide-tag");
         this.draw_radial_tree("#g" + position);
     }
 
-    
-
-    d3.select("#g"+position).attr("viewBox", function(){
+    d3.select("#g" + position).attr("viewBox", function () {
         return "0 0 500 500";
-    })
+    });
 }
 
 var FileName;
 var dataSourcePapers, dataSourceCitations;
 
-window.onload = function(){
-    var fader = function(color) { return d3.interpolateRgb(color, "#fff")(0.2); };
+window.onload = function () {
+    var fader = function (color) {
+        return d3.interpolateRgb(color, "#fff")(0.2);
+    };
     var colors = d3.schemeCategory20.map(fader);
-    
+
     treeLib.shuffleArray(colors);
-    
+
     color = d3.scaleOrdinal(colors);
 
     FileName = menu.changeDataset(FileName, 1);
 
     menu.dataTypeSpanText();
-    
+
     loadVisualization("1");
     loadVisualization("2");
-        
-}
+};
 
 function updateDataset() {
     FileName = menu.changeDataset(FileName);
-    
+
     menu.dataTypeSpanText();
     // allow user to select from intermediate levels of nodes
     // and only display them in the graph
@@ -174,26 +165,26 @@ function updateDataset() {
     // 2. make a collection of of distinct nodes from each level
     // 3. build a multiselect for each level from the nodes
     // 4. filter dataset on load of json
-    dataFilterSubset()
+    dataFilterSubset();
 
-    var locked1 = menu.isLocked('1');
-    var locked2 = menu.isLocked('2');
+    var locked1 = menu.isLocked("1");
+    var locked2 = menu.isLocked("2");
 
-    loadVisualization('1', locked1);
+    loadVisualization("1", locked1);
 
-    loadVisualization('2', locked2);
+    loadVisualization("2", locked2);
 }
 
 function dataFilterSubset() {
-    d3.json(FileName, function(error, data) {
+    d3.json(FileName, function (error, data) {
         debugger;
     });
 }
 
 function setupSliderValueTooltip() {
-    sidebarSliderDivs = ['#nodesizeScalarDiv', '#nodeDegreeDiv', '#highlightOpacityDiv'];
+    sidebarSliderDivs = ["#nodesizeScalarDiv", "#nodeDegreeDiv", "#highlightOpacityDiv"];
     sliderDivs = sidebarSliderDivs;
-    $.each(sliderDivs, function(index, sliderDiv){
+    $.each(sliderDivs, function (index, sliderDiv) {
         setupEachSlider(sliderDiv);
     });
 }
@@ -201,56 +192,53 @@ function setupSliderValueTooltip() {
 // set up one slider's value displaying effect
 function setupEachSlider(sliderDivName) {
     var sliderDiv = $(sliderDivName),
-      slider = $('input', sliderDiv),
-      valueText = $('.sliderValue', sliderDiv),
-      thumbwidth = 20;
-  
+        slider = $("input", sliderDiv),
+        valueText = $(".sliderValue", sliderDiv),
+        thumbwidth = 20;
+
     function setTooltip() {
-      var value = slider.val();
-      var percent = (value - slider.attr('min'))/(slider.attr('max') - slider.attr('min'));
-      var thumbCorrect = thumbwidth * (percent - 0.5) * -1,
-        textPos = Math.round((percent * slider.width()) - thumbwidth/4 + thumbCorrect);
-      valueText.css('left', textPos);
-      valueText.css('top', 25);
-      valueText.text(value);
+        var value = slider.val();
+        var percent = (value - slider.attr("min")) / (slider.attr("max") - slider.attr("min"));
+        var thumbCorrect = thumbwidth * (percent - 0.5) * -1,
+            textPos = Math.round(percent * slider.width() - thumbwidth / 4 + thumbCorrect);
+        valueText.css("left", textPos);
+        valueText.css("top", 25);
+        valueText.text(value);
     }
-  
+
     function setSliderEvent() {
-      slider.on('input.slider change.slider keyup.slider mouseover.slider', function() {
-        setTooltip();
-        valueText.css('visibility', 'visible');
-      });
-  
-      slider.on('mouseout.slider', function() {
-        valueText.css('visibility', 'hidden');
-      });
-  
-      // when window size changes
-      $(window).on('resize.slider', function() {
-        setTooltip();
-      });
+        slider.on("input.slider change.slider keyup.slider mouseover.slider", function () {
+            setTooltip();
+            valueText.css("visibility", "visible");
+        });
+
+        slider.on("mouseout.slider", function () {
+            valueText.css("visibility", "hidden");
+        });
+
+        // when window size changes
+        $(window).on("resize.slider", function () {
+            setTooltip();
+        });
     }
-  
+
     setSliderEvent();
 }
 
 /**
  * initiates the hover tool tips
  */
-function setupToolTips()
-{
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    // initiate footer tooltip, and set hidden
-    //updateFooterText(TOOLTIP.SVG_HOVER, "hidden");
-  });
+function setupToolTips() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+        // initiate footer tooltip, and set hidden
+        //updateFooterText(TOOLTIP.SVG_HOVER, "hidden");
+    });
 }
 
 /**
  * mouseover and mouseout handler for showing and hiding tooltips
  */
-function mouseOverOut(element, text)
-{
-  element.on("mouseover", () => updateFooterText(text, "visible"))
-    .on("mouseout", () => updateFooterText(text, "hidden"));
+function mouseOverOut(element, text) {
+    element.on("mouseover", () => updateFooterText(text, "visible")).on("mouseout", () => updateFooterText(text, "hidden"));
 }
--- js/sunburst.js
+++ js/sunburst.js
@@ -1,4 +1,4 @@
-function draw_sunburst(position){
+function draw_sunburst(position) {
     var position2 = position == "#g1" ? "g2" : "g1";
     var position1 = position == "#g1" ? "g1" : "g2";
 
@@ -6,147 +6,160 @@ function draw_sunburst(position){
 
     var width = 500,
         height = 500,
-        radius = (Math.min(width, height) / 2) - 10;
+        radius = Math.min(width, height) / 2 - 10;
 
     var formatNumber = d3.format(",d");
 
-    var x = d3.scaleLinear()
-        .range([0, 2 * Math.PI]);
+    var x = d3.scaleLinear().range([0, 2 * Math.PI]);
 
-    var y = d3.scaleSqrt()
-        .range([0, radius]);
+    var y = d3.scaleSqrt().range([0, radius]);
 
     var partition = d3.partition();
 
-    var arc = d3.arc()
-        .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
-        .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
-        .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
-        .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+    var arc = d3
+        .arc()
+        .startAngle(function (d) {
+            return Math.max(0, Math.min(2 * Math.PI, x(d.x0)));
+        })
+        .endAngle(function (d) {
+            return Math.max(0, Math.min(2 * Math.PI, x(d.x1)));
+        })
+        .innerRadius(function (d) {
+            return Math.max(0, y(d.y0));
+        })
+        .outerRadius(function (d) {
+            return Math.max(0, y(d.y1));
+        });
 
+    var svg = d3
+        .select(position)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var svg = d3.select(position)
-    .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
-
-    d3.json(FileName, function(error, data) {
+    d3.json(FileName, function (error, data) {
         if (error) throw error;
         // FILTER JSON
-        data.children = data.children.filter(function(el, i){ if(i<10){ return el }})
-        
-        var root = d3.hierarchy(data)
-            .sum(function(d) { return d.size; })
-            .sort(function(a, b) { return b.value - a.value; });
+        data.children = data.children.filter(function (el, i) {
+            if (i < 10) {
+                return el;
+            }
+        });
 
-        // preserve the accSize for citations    
-        root = treeLib.preserveAccSize(root);        
+        var root = d3
+            .hierarchy(data)
+            .sum(function (d) {
+                return d.size;
+            })
+            .sort(function (a, b) {
+                return b.value - a.value;
+            });
+
+        // preserve the accSize for citations
+        root = treeLib.preserveAccSize(root);
 
         // process the value as either leaves or acc size depending on control panel
         root = menu.processAccumulated(root);
 
         svg.selectAll("path")
             .data(partition(root).descendants())
-            .enter().append("path")
-            .attr("d", function(d){
+            .enter()
+            .append("path")
+            .attr("d", function (d) {
                 // if collapsible tree comparison or zoomable tree map!!!
                 // only show the root and first level to match these
-                if (treeLib.restrictLevels(d, 2, position2))
-                    return;
+                if (treeLib.restrictLevels(d, 2, position2)) return;
 
                 return arc(d);
             })
-            .style("fill", function(d) {
-                if (!d.parent)
-                    return "#e6e6e6";
-                else
-                    return treeLib.getColor(d, color);
+            .style("fill", function (d) {
+                if (!d.parent) return "#e6e6e6";
+                else return treeLib.getColor(d, color);
             })
             .style("stroke", "white")
             .style("stroke-width", 0.5)
-            .attr("id", d => {
+            .attr("id", (d) => {
                 return treeLib.pathId(d, position1);
             })
-            .attr("class", d => {
-                var cls = treeLib.isLeaf(d) ? 'leaf cursor-default' : '';
+            .attr("class", (d) => {
+                var cls = treeLib.isLeaf(d) ? "leaf cursor-default" : "";
 
                 return cls;
             })
             .classed(position1, true)
-            .on("mouseover", d => treeLib.mouseoverLinking(position1, position2, d))
-            .on("mouseout", d => treeLib.mouseoutLinking(position1, position2, d))
-            .on('linkedClick', d => linkedClickClick(d))
-            .on("click", d => {
+            .on("mouseover", (d) => treeLib.mouseoverLinking(position1, position2, d))
+            .on("mouseout", (d) => treeLib.mouseoutLinking(position1, position2, d))
+            .on("linkedClick", (d) => linkedClickClick(d))
+            .on("click", (d) => {
                 treeLib.displaySelectedNode(d);
 
                 var prevent = treeLib.preventLeaf(d, position1);
 
-                if (prevent)
-                    return;
+                if (prevent) return;
 
                 click(d);
             })
             .append("title")
-            .text(d => {
+            .text((d) => {
                 var dataName = d.data.name;
 
-                return dataName + "\n" + menu.dataInfoSizeText(d.accSize)
+                return dataName + "\n" + menu.dataInfoSizeText(d.accSize);
             });
 
-            d3.select("svg#"+position1).dispatch('doneDrawing');
+        d3.select("svg#" + position1).dispatch("doneDrawing");
     });
-    
+
     function click(d) {
         var response = treeLib.linkedClick(d, position2);
 
-        if (response == 'prevent')
-            return;
+        if (response == "prevent") return;
 
         svg.transition()
             .duration(750)
-            .tween("scale", function() {
+            .tween("scale", function () {
                 var ccn = treeLib.config().containers[1].currentClickedNode;
                 var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
                     yd = d3.interpolate(y.domain(), [d.y0, 1]),
                     yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
 
-                
-
-                return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
-            
+                return function (t) {
+                    x.domain(xd(t));
+                    y.domain(yd(t)).range(yr(t));
+                };
             })
-        .selectAll("path")
-            .attrTween("d", function(d) { 
-                return function() {
+            .selectAll("path")
+            .attrTween("d", function (d) {
+                return function () {
                     // returns the arc(d) for acceptable things
                     // to show the only first level on return to root CT & ZT
-                    return treeLib.restrictOnReturn(d, position1, position2, arc)
-                }; 
+                    return treeLib.restrictOnReturn(d, position1, position2, arc);
+                };
             });
     }
 
     function linkedClickClick(d) {
         svg.transition()
             .duration(750)
-            .tween("scale", function() {
-            var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-                yd = d3.interpolate(y.domain(), [d.y0, 1]),
-                yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
-            return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+            .tween("scale", function () {
+                var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+                    yd = d3.interpolate(y.domain(), [d.y0, 1]),
+                    yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+                return function (t) {
+                    x.domain(xd(t));
+                    y.domain(yd(t)).range(yr(t));
+                };
             })
-        .selectAll("path")
-            .attrTween("d", function(d) { 
-                return function() { 
-                    // return arc(d); 
+            .selectAll("path")
+            .attrTween("d", function (d) {
+                return function () {
+                    // return arc(d);
                     return treeLib.restrictOnReturn(d, position1, position2, arc);
-                }; 
+                };
             })
             .style("visibility", "visible")
-            .style("visibility", function(d) {
+            .style("visibility", function (d) {
                 return treeLib.hideArcs(position1, d);
-            })
-
+            });
     }
-    
-    d3.select(self.frameElement).style("height", height + "px");
 
+    d3.select(self.frameElement).style("height", height + "px");
 }
