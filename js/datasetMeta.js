1cc6fdfc4980375dcdbcb02c333e77c7f5166cb6
iamwyh2019 <799810767@qq.com>
2021-07-12 23:29:33 +0800

    Add a metafile to describe dataset properties
    Add a file `js/datasetMeta.js` to indicate the <option> tag properties, such as id, label, data, value, and whether it is for study or for mock quiz.

---
+++ js/datasetMeta.js
@@ -0,0 +1,122 @@
+var datasetDescription = [
+    {
+        "label": "Publication",
+        "id": "publication",
+        "options": [
+            {
+                "value": "Faloutsos",
+                "name": "Christos Faloutsos",
+                "data": "author",
+                "mode": "Study"
+            },
+            {
+                "value": "Han",
+                "name": "Jiawei Han",
+                "data": "author",
+                "mode": "Study" 
+            },
+            {
+                "value": "Stasko",
+                "name": "John Stasko",
+                "data": "author",
+                "mode": "Mock Quiz"
+            },
+            {
+                "value": "Leskovec",
+                "name": "Jure Leskovec",
+                "data": "author",
+                "mode": "Mock Quiz"
+            }
+        ]
+    },
+    {
+        "label": "Government",
+        "id": "government",
+        "options": [
+            {
+                "value": "Gov_Canada",
+                "name": "Canada",
+                "data": "government",
+                "mode": "Study"
+            },
+            {
+                "value": "Gov_Philippines",
+                "name": "Philippines",
+                "data": "government",
+                "mode": "Study"
+            },
+            {
+                "value": "Gov_United_Kingdom",
+                "name": "United Kingdom",
+                "data": "government",
+                "mode": "Mock Quiz"
+            },
+            {
+                "value": "Gov_United_States",
+                "name": "United States",
+                "data": "government",
+                "mode": "Mock Quiz"
+            }
+        ]
+    },
+    {
+        "label": "Trade",
+        "id": "trade",
+        "options": [
+            {
+                "value": "Trade_China",
+                "name": "China",
+                "data": "trade",
+                "mode": "Study"
+            },
+            {
+                "value": "Trade_Germany",
+                "name": "Germany",
+                "data": "trade",
+                "mode": "Study"
+            },
+            {
+                "value": "Trade_United_Kingdom",
+                "name": "United Kingdom",
+                "data": "trade",
+                "mode": "Mock Quiz"
+            },
+            {
+                "value": "Trade_United_States",
+                "name": "United States",
+                "data": "trade",
+                "mode": "Mock Quiz"
+            }
+        ]
+    },
+    {
+        "label": "Tree Taxonomy",
+        "id": "treeoflife",
+        "options": [
+            {
+                "value": "TreeOfLife_Anura",
+                "name": "Anura (Frogs & Toads)",
+                "data": "treeoflife",
+                "mode": "Study"
+            },
+            {
+                "value": "TreeOfLife_Carnivora",
+                "name": "Carnivora (Cats & Dogs)",
+                "data": "treeoflife",
+                "mode": "Study"
+            },
+            {
+                "value": "TreeOfLife_Pelecaniformes",
+                "name": "Pelecaniformes (Waterbirds)",
+                "data": "treeoflife",
+                "mode": "Mock Quiz"
+            },
+            {
+                "value": "TreeOfLife_Afrosoricida",
+                "name": "Afrosoricida (Moles, Shrews & Tenrecs)",
+                "data": "treeoflife",
+                "mode": "Mock Quiz"
+            }
+        ]
+    }
+];
--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var dataGlobal,
     leavesGlobals,
     dataFilterSubsetGlobal;
 let MODE = "Study";
-let datasetMetaFile = './datasets/datasetMeta.json';
 
 if (confirm("Would you like to take the mock quiz?"))
     MODE = 'Mock Quiz';
@@ -24,8 +23,6 @@ $(document).ready(function () {
     setupToolTips();
 
     setupOnboarding();
-
-    menu.dataFilterSubset();
 
     if (MODE == "Mock Quiz") {
         mockQuiz.hideDataSelect();
@@ -175,32 +172,49 @@ window.onload = function () {
 function updateDataTypeAndFileName() {
     // get the filename and datatype for the initial dataset
 
-    d3.json(datasetMetaFile, (e,d) => console.log(d));
+    d3.select('#dataDropdown')
+        .selectAll('optgroup')
+        .data(datasetDescription)
+        .enter().append('optgroup')
+        .attr("label", d => d.label)
+        .attr("id", d => d.id)
+        .selectAll('option')
+        .data(d => d.options.filter(op => MODE == "Study"? op.mode == MODE : true))
+        .enter().append('option')
+        .attr('value', d => d.value)
+        .attr('data', d => d.data)
+        .text(d => d.name);
     
-    var objD = document.getElementById("dataDropdown");
+    let initialSelected = d3.select('#dataDropdown').select('option');
+    initialSelected.attr('selected', 'selected');
 
-    var dataType = d3.select(objD.selectedOptions[0]).attr("data");
+    let selectedDataType = initialSelected.attr('data');
+    let selectedDataValue = initialSelected.attr('value');
 
-    // update the stored dataType in menu config
-    menu.updateConfig("dataType", dataType)
+    menu.updateConfig("dataType", selectedDataType);
+    treeLib.displayedNode(selectedDataValue);
+    let filename = "datasets/" + selectedDataValue + ".txt";
+    menu.updateConfig("filename", filename);
+    menu.dataFilterSubset();
 
-    // update the selected node in treeLib
-    treeLib.displayedNode(objD.value);
-
-    var filename = "datasets/" + objD.value + ".txt";
-
-    // update the stored filename in menu config
-    menu.updateConfig("filename", filename)
 }
 
 function updateDataset() {
-    updateDataTypeAndFileName();
+
     // allow user to select from intermediate levels of nodes
     // and only display them in the graph
     // 1. iterate through data
     // 2. make a collection of of distinct nodes from each level
     // 3. build a multiselect for each level from the nodes
     // 4. filter dataset on load of json
+
+    let objD = $('#dataDropdown option:selected');
+    let selectedDataValue = objD.attr('value');
+    let filename = "datasets/" + selectedDataValue + ".txt";
+    menu.updateConfig("filename", filename);
+
+    let selectedDataType = objD.attr('data');
+    menu.updateConfig("dataType", selectedDataType);
 
     menu.dataFilterSubset();
 
